b39f9f727f2f3677136cc25b4df18f35868890ed
chongct <chongchingtong@gmail.com>
2025-06-22 22:21:53 +0800

    Implement auth registration

--- src/app/ui/form/PasswordInput.tsx
+++ src/app/ui/form/PasswordInput.tsx
@@ -1,19 +1,22 @@
+import clsx from 'clsx';
+
 import styles from '@/app/ui/icons.module.css';
 
 type PasswordInputProps = {
+  error?: string;
   id?: string;
   labelText?: string;
 };
 
-export async function PasswordInput(props: PasswordInputProps) {
-  const { id = 'password', labelText = 'Password' } = props;
+export function PasswordInput(props: PasswordInputProps) {
+  const { error, id = 'password', labelText = 'Password' } = props;
 
   return (
     <>
       <label className="uppercase text-sm font-semibold tracking-wider" htmlFor={id}>
         {labelText}
       </label>
-      <div className="mt-3 relative">
+      <div className={clsx('mt-3', 'relative', error && 'mb-2')}>
         <div className={styles.lockIcon} />
         <input
           id={id}
@@ -25,6 +28,12 @@ export async function PasswordInput(props: PasswordInputProps) {
           className="w-full h-10 bg-[var(--background)] pl-10 py-2 rounded-xl"
         />
       </div>
+      {error && (
+        <div className="relative h-5 text-[var(--warning)] text-sm font-medium">
+          <div className={styles.alertIcon} />
+          <span className="absolute left-6">{error}</span>
+        </div>
+      )}
     </>
   );
 }
