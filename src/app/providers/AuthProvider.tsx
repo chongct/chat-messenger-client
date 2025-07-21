995390fd2d98c37db5b823ab06a10795acd0d902
chongct <chongchingtong@gmail.com>
2025-07-22 01:05:46 +0800

    Use jwt and headers for csrf

--- src/app/providers/AuthProvider.tsx
+++ src/app/providers/AuthProvider.tsx
@@ -19,6 +19,7 @@ interface IAuthContext {
   setAccessToken: Dispatch<SetStateAction<string>>;
   userId: string;
   setUserId: Dispatch<SetStateAction<string>>;
+  tempCsrf: string;
 }
 
 const AuthContext = createContext<IAuthContext>({
@@ -27,16 +28,19 @@ const AuthContext = createContext<IAuthContext>({
   setAccessToken: noop,
   userId: '',
   setUserId: noop,
+  tempCsrf: '',
 });
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const [userId, setUserId] = useState('');
   const [loading, setLoading] = useState(true);
   const [accessToken, setAccessToken] = useState('');
+  const [tempCsrf, setTempCsrf] = useState('');
 
   const attemptRefreshToken = async () => {
-    await getCsrfToken();
-    const refreshStatus = await getRefreshToken();
+    const csrfTokenResponse = await getCsrfToken();
+    const { token: csrfToken } = csrfTokenResponse ?? {};
+    const refreshStatus = await getRefreshToken(csrfToken);
     const { accessToken, refreshToken: refreshTokenFromResponse, userId } = refreshStatus ?? {};
 
     if (accessToken && userId) {
@@ -44,6 +48,8 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
       setUserId(userId);
       localStorage.setItem(REFRESH_TOKEN_LOCAL_STORAGE_KEY, refreshTokenFromResponse);
     }
+
+    setTempCsrf(csrfToken);
   };
 
   useEffect(() => {
@@ -75,7 +81,9 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
   }, [accessToken]);
 
   return (
-    <AuthContext.Provider value={{ loading, accessToken, setAccessToken, userId, setUserId }}>
+    <AuthContext.Provider
+      value={{ loading, accessToken, setAccessToken, userId, setUserId, tempCsrf }}
+    >
       {children}
     </AuthContext.Provider>
   );
